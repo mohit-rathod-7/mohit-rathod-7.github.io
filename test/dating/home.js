faefffddaca085f404062c0b8e3e6fbf0ac4d2ac
mohit-rathod-7 <108727193+mohit-rathod-7@users.noreply.github.com>
2022-07-13 15:41:38 +0530

    Update home.js

--- test/dating/home.js
+++ test/dating/home.js
@@ -26,10 +26,10 @@ $(document).ready(function() {
 });
 
 
-const sideNav = gsap.timeline();
+const cardInfo = gsap.timeline();
 
-$('.more').click(function(){
-    sideNav
+$('.swiper-info').click(function(){
+    cardInfo
     .from('.card-info', { ease: 'power2.in', duration: 1, y: '100%', stagger: 0.15 })
 
     TweenLite.to(".card-info", 0.5, {ease: Back.easeInOutSine, force3D: true, x: '0%', display: 'block' });
