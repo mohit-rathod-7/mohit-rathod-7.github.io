cef00d2d0fbfc4a544a9eccea9edad431cbe9fb4
mohit-rathod-7 <108727193+mohit-rathod-7@users.noreply.github.com>
2022-07-19 04:54:09 +0530

    Update home.js

--- test/dating/home.js
+++ test/dating/home.js
@@ -16,9 +16,13 @@ $(document).ready(function() {
 
 $("#home").addClass("disabled");
 
+function scrollTopAnimated() {
+    $(".card-info").scrollTop(0);
+}
+
 // SET TIMELINE
 const cardInfo = gsap.timeline();
-var animationDuration = 0.5;
+var animationDuration = 1;
 
 // TO OPEN .card-info BOX
 $('.swiper-info').click(function(){
@@ -34,9 +38,10 @@ $('.swiper-info').click(function(){
 // TO CLOSE .card-info BOX
 $('.card-close .icon').click(function(){
     cardInfo
+    .call(scrollTopAnimated())
     .to('.card-info', { ease: 'power2.in', duration: animationDuration, y: '100%' })
     .set('.swiper', { pointerEvents: 'all' })
-
-    TweenLite.to(".card-info", animationDuration, {ease: Back.easeInOutSine, force3D: true, display: 'none' });
-    TweenLite.to(".swiper", animationDuration, {ease: Back.easeInOutSine, opacity: '1' });
+    
+    TweenLite.to(".card-info", animationDuration*2, {ease: Back.easeInOutSine, force3D: true, display: 'none' });
+    TweenLite.to(".swiper", animationDuration*2, {ease: Back.easeInOutSine, opacity: '1' });
 })
